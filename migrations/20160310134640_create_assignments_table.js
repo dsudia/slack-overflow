fc1c8cfbaed6802a12006b140779fb79fd25d445
David Sudia <dsudia@gmail.com>
2016-03-10 14:06:42 -0700

    add assignments migration instructions

--- migrations/20160310134640_create_assignments_table.js
+++ migrations/20160310134640_create_assignments_table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('assignments', function(table) {
+    table.increments();
+    table.string('name').unique();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('assignments');
 };
