74be0f8dc06989bfd40494ecd48d782a030363f1
David Sudia <dsudia@gmail.com>
2016-03-10 14:18:19 -0700

    add tags migration instructions

--- migrations/20160310134741_create_tags_table.js
+++ migrations/20160310134741_create_tags_table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('tags', function(table) {
+    table.increments();
+    table.string('tag_name').unique();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('tags');
 };
