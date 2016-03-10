1f1e93461938f2bc1d84bf935505fa91e5626846
David Sudia <dsudia@gmail.com>
2016-03-10 13:58:05 -0700

    add groups migration instructions

--- migrations/20160310134631_create_groups_table.js
+++ migrations/20160310134631_create_groups_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('groups', function(table) {
+    table.increments();
+    table.string('name');
+    table.string('slack_channel');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('groups');
 };
