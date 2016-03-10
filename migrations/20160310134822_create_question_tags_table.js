c9fdf46eb2639a750414046355ff1aff89b5536a
David Sudia <dsudia@gmail.com>
2016-03-10 14:21:00 -0700

    add question_tags and subscriptions migration instructions

--- migrations/20160310134822_create_question_tags_table.js
+++ migrations/20160310134822_create_question_tags_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('question_tags', function(table) {
+    table.increments();
+    table.integer('question_id').references('questions.id');
+    table.integer('tag_id').references('tags.id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('question_tags');
 };
--- migrations/20160310134908_create_subscriptions_table.js
+++ migrations/20160310134908_create_subscriptions_table.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('subscriptions', function(table) {
+    table.increments();
+    table.integer('user_id').references('users.id');
+    table.integer('question_id').references('questions.id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('subscriptions');
 };
