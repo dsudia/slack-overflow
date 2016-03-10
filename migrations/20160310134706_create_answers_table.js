c2d9e84d4c9ac40d8f565ad205c2eb115d7027ae
David Sudia <dsudia@gmail.com>
2016-03-10 14:15:40 -0700

    add answers migration instructions

--- migrations/20160310134706_create_answers_table.js
+++ migrations/20160310134706_create_answers_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('answers', function(table) {
+    table.increments();
+    table.string('title');
+    table.string('body', 3000);
+    table.integer('user_id').references('users.id');
+    table.integer('score');
+    table.boolean('flag_status').defaultTo('false');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('answers');
 };
