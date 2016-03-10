da2ff6eac758128a79731aff81b6416fba6ca359
David Sudia <dsudia@gmail.com>
2016-03-10 14:02:49 -0700

    move assignments table above questions table in migrations

--- migrations/20160310134650_create_questions_table.js
+++ migrations/20160310134650_create_questions_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('questions', function(table) {
+    table.increments();
+    table.string('title');
+    table.string('body', 3000);
+    table.integer('group_id').references('groups.id');
+    table.integer('user_id').references('users.id');
+    table.integer('score');
+    table.boolean('flag_status').defaultTo('false');
+    table.integer('assignment_id').references('assignments.id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+
 };
