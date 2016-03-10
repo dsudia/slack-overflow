eed56dd996a59508b3839c1e27932b0ba244c025
David Sudia <dsudia@gmail.com>
2016-03-10 13:56:45 -0700

    add users migration instructions

--- migrations/20160310134620_create_users_table.js
+++ migrations/20160310134620_create_users_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.integer('auth_id').references('auth_levels.id').notNullable();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
+    table.string('email').unique().notNullable();
+    table.integer('score');
+    table.string('slack_id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
