5f6c2f6d528ccf5b0416bb301af04a6244b78e27
David Sudia <dsudia@gmail.com>
2016-03-10 13:52:20 -0700

    add instructions to auth_levels migration

--- migrations/20160310134604_create_auth_levels_table.js
+++ migrations/20160310134604_create_auth_levels_table.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('auth_levels', function(table) {
+    table.increments();
+    table.string('level').unique().notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('auth_levels');
 };
