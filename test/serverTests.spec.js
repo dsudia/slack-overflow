dd4f9e7a31f886c526a912d1f866d2c8a833c909
David Sudia <dsudia@gmail.com>
2016-03-10 17:10:07 -0700

    added list of initial tests to write

--- test/serverTests.spec.js
+++ test/serverTests.spec.js
@@ -6,3 +6,26 @@ var knex = require('../db/knex');
 var should = chai.should();
 
 chai.use(chaiHttp);
+
+
+// test for getting index.html
+
+// test for logging in as a student user
+
+// test for viewing a question's page
+  // question should load
+  // answer should load
+
+// test for registering as a student user
+
+// test for logging out a user
+
+// test that a user has all necessary info
+
+// test that questions sort correctly
+
+// test that questions have tags
+
+// test that voting on a question works
+
+// test that only registered users can view certain pages
