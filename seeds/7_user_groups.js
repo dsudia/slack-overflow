46af1a462d464d5619069b7ca227ca56432eeda5
David Sudia <dsudia@gmail.com>
2016-03-13 13:38:50 -0600

    populate user_groups seed file

--- seeds/7_user_groups.js
+++ seeds/7_user_groups.js
@@ -2,11 +2,14 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('user_groups').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('user_groups').insert({user_id: knex('users').select('id').where('username', 'dsudia'),
+      group_id: knex('groups').select('id').where('name', 'G19')}),
+    knex('user_groups').insert({user_id: knex('users').select('id').where('username', 'mhea0'),
+      group_id: knex('groups').select('id').where('name', 'G19')}),
+    knex('user_groups').insert({user_id: knex('users').select('id').where('username', 'kkawa'),
+      group_id: knex('groups').select('id').where('name', 'G19')})
   );
 };
