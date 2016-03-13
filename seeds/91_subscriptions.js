fd4d8598c71b6e2fa4bcafe25858f6725b475fcc
David Sudia <dsudia@gmail.com>
2016-03-13 13:44:29 -0600

    populate subscriptions seed file

--- seeds/91_subscriptions.js
+++ seeds/91_subscriptions.js
@@ -2,11 +2,10 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('subscriptions').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('subscriptions').insert({user_id: knex('users').select('id').where('username', 'dsudia'),
+      question_id: knex('questions').select('id').where('title', 'Not sure how to select all from a table with parameters...')})
   );
 };
