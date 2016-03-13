dfa8042e96b7e22a3cdacf333b90ab0278495a78
David Sudia <dsudia@gmail.com>
2016-03-13 13:42:40 -0600

    populate question_tags seed file

--- seeds/9_question_tags.js
+++ seeds/9_question_tags.js
@@ -2,11 +2,10 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('question_tags').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('question_tags').insert({question_id: knex('questions').select('id').where('title', 'Not sure how to select all from a table with parameters...'),
+      tag_id: knex('tags').select('id').where('tag_name', 'knex')})
   );
 };
