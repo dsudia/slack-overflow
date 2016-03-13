229d987b82674a1fb7eb6297ccad9449304de8e7
David Sudia <dsudia@gmail.com>
2016-03-13 11:57:59 -0600

    populate auth_levels seed file

--- seeds/1_auth_levels.js
+++ seeds/1_auth_levels.js
@@ -2,11 +2,11 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('table_name').del(), 
+    knex('auth_levels').del(),
 
     // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
+    knex('auth_levels').insert({level: 'admin'}),
+    knex('auth_levels').insert({level: 'instructor'}),
+    knex('auth_levels').insert({level: 'student'})
   );
 };
