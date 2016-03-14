0443b070030a998d6099d29f957ef989e6a4a4e7
David Sudia <dsudia@gmail.com>
2016-03-14 13:52:51 -0600

    commit to rebase from master

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -18,6 +18,25 @@ router.get('/questions/new', function(req, res, next) {
   res.render('newQuestion', {title: 'Slack Overflow - Post a Question'});
 });
 
+router.post('/questions/new', function(req, res, next) {
+  // store form info in a variable
+  var qData = req.body;
+  // do table insert
+  knex('questions').insert({title: qData.title,
+    body: qData.body,
+    group_id: qData.group_id,
+    user_id: qData.user_id,
+    score: 0,
+    flag_status:false,
+    assignment_id: qData.assignment_id}).then(function() {
+      //retrieve id of new question
+      knex('questions').select('id').where('body', qData.body);
+    }).then(function(data) {
+      //render question page
+      res.render('/questions/' + data[0]);
+    });
+});
+
 
 
 module.exports = router;
