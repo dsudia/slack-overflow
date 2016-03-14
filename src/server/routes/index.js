14c372375cb187e484a0c143c514046b5c91b06d
David Sudia <dsudia@gmail.com>
2016-03-14 14:18:58 -0600

    insert function works correctly and brings back correct info

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -21,6 +21,8 @@ router.get('/questions/new', function(req, res, next) {
 router.post('/questions/new', function(req, res, next) {
   // store form info in a variable
   var qData = req.body;
+  console.log(req.body);
+  console.log(qData.title);
   // do table insert
   knex('questions').insert({title: qData.title,
     body: qData.body,
@@ -28,10 +30,8 @@ router.post('/questions/new', function(req, res, next) {
     user_id: qData.user_id,
     score: 0,
     flag_status:false,
-    assignment_id: qData.assignment_id}).then(function() {
-      //retrieve id of new question
-      knex('questions').select('id').where('body', qData.body);
-    }).then(function(data) {
+    assignment_id: qData.assignment_id}, 'id').then(function(data) {
+      console.log(data);
       //render question page
       res.render('/questions/' + data[0]);
     });
