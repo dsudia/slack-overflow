07ed6f7a3d217df89f47383e63a58b2b629d389b
David Sudia <dsudia@gmail.com>
2016-03-15 17:25:18 -0600

    put dotenv config back in and changed database location in knexfile to actual url

--- src/server/app.js
+++ src/server/app.js
@@ -16,6 +16,7 @@ var SlackStrategy = require('passport-slack').Strategy;
 var knex = require('../../db/knex');
 var cookieSession = require('cookie-session');
 var helpers = require('./lib/helpers');
+if ( !process.env.NODE_ENV ) { require('dotenv').config(); }
 
 
 // *** routes *** //
