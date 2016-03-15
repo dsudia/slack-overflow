ab4e18f90b4376efd669958566dd310baea79e2c
David Sudia <dsudia@gmail.com>
2016-03-15 17:28:10 -0600

    tried adding ssl to actual url

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://canftibofebrtp:lIViDJkzfDUWPdkL4jIW5cnDms@ec2-54-83-56-177.compute-1.amazonaws.com:5432/d9baposi4fo3eo'
+    connection: 'postgres://canftibofebrtp:lIViDJkzfDUWPdkL4jIW5cnDms@ec2-54-83-56-177.compute-1.amazonaws.com:5432/d9baposi4fo3eo?ssl=true'
   }
 };
