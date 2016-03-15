8183442007db41611a41838a01e69522593fb5a7
David Sudia <dsudia@gmail.com>
2016-03-14 19:28:59 -0600

    fixed title on one question

--- seeds/5_questions.js
+++ seeds/5_questions.js
@@ -12,7 +12,7 @@ exports.seed = function(knex, Promise) {
       score: 1,
       flag_status: false,
       assignment_id: knex('assignments').select('id').where('name', 'Introduction to Knex')}),
-    knex('questions').insert({title: 'Help! I can\'t figure out CSS positioning! ',
+    knex('questions').insert({title: 'Help! I can\'t figure out CSS positioning!',
       body: 'Relative and absolute and fixed, oh my! Can anyone give me a good explanation of the differences?',
       group_id: knex('groups').select('id').where('name', 'G18'),
       user_id: knex('users').select('id').where('username', 'mhea0'),
