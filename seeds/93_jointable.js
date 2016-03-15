9ff5b6391cba8984cd2b9b9657f1862f2a7e1881
David Sudia <dsudia@gmail.com>
2016-03-15 16:00:46 -0600

    Update 93_jointable.js

--- seeds/93_jointable.js
+++ seeds/93_jointable.js
@@ -2,15 +2,15 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('join_table').del(), 
+    knex('class_info').del(), 
 
     // Inserts seed entries
-    knex('join_table').insert({week_id: 1, cohort_id: 2, assign_id: 1}),
-    knex('join_table').insert({week_id: 2, cohort_id: 2, assign_id: 2}),
-    knex('join_table').insert({week_id: 10, cohort_id: 1, assign_id: 3}),
-    knex('join_table').insert({week_id: 10, cohort_id: 1, assign_id: 4}),
-    knex('join_table').insert({week_id: 10, cohort_id: 1, assign_id: 5}),
-    knex('join_table').insert({week_id: 10, cohort_id: 1, assign_id: 6}),
-    knex('join_table').insert({week_id: 10, cohort_id: 1, assign_id: 7})
+    knex('class_info').insert({week_id: 1, cohort_id: 2, assign_id: 1}),
+    knex('class_info').insert({week_id: 2, cohort_id: 2, assign_id: 2}),
+    knex('class_info').insert({week_id: 10, cohort_id: 1, assign_id: 3}),
+    knex('class_info').insert({week_id: 10, cohort_id: 1, assign_id: 4}),
+    knex('class_info').insert({week_id: 10, cohort_id: 1, assign_id: 5}),
+    knex('class_info').insert({week_id: 10, cohort_id: 1, assign_id: 6}),
+    knex('class_info').insert({week_id: 10, cohort_id: 1, assign_id: 7})
   );
 };
