5f31c1ca59076850e526360bcaff53f74d108ef5
David Sudia <dsudia@gmail.com>
2016-03-14 18:49:27 -0600

    tags show up on page correctly

--- src/server/lib/helpers.js
+++ src/server/lib/helpers.js
@@ -1,24 +1,70 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-var knex = require('../../../db/knex');
-var helpers = require('./helpers');
+var bcrypt = require('bcrypt');
+var Promise = require('bluebird');
+Promise.promisifyAll(bcrypt);
 
+module.exports = {
+  ensureAuthenticated: function(req, res, next) {
+    // check if user is authenticated
+    if(req.user) {
+      // if so -> call next()
+      return next();
+    } else if (!req.user) {
+      // if not -> redirect to login
+      req.flash('error', 'You need to login!');
+      return res.redirect('/login');
+    }
+  },
 
-passport.use(new LocalStrategy({
-},
-  function(username, password, done) {
-    knex('users').where('username', username)
-      .then(function(data) {
-        var user = data[0];
-        if (password === user.password) {
-          return done(null, user, {message: 'You\'re logged in!'});
-        } else {
-          return done(null, false, {message: 'Incorrect password.'});
-        }
+  ensureAdmin: function(req, res, next) {
+    // check if user is admin
+    if(req.user.admin === true) {
+      // if so call next()
+      return next();
+    } else {
+      // if not, redirect to login
+      req.flash('error', 'You are not logged in as an admin.');
+      return res.status(403).redirect('/login');
+    }
+  },
+
+  loginRedirect: function(req, res, next) {
+    // check if user is authenticated
+    if(req.user) {
+      // if so -> redirect to main route
+      return res.redirect('/');
+    } else {
+      // if not -> call next()
+      return next();
+    }
+  },
+
+  hashString: function(string) {
+    return bcrypt.hash(string, 10, function(err, hash) {
+      return hash;
+    }).then(function(hashedString) {
+      return hashedString;
     }).catch(function(err) {
-      return done(null, false, {message: 'Incorrect username.'});
+      return 'Hashing process failed: ' + err;
     });
-  }
-));
+  },
 
-module.exports = passport;
+  checkPassword: function (pass, hashedPass) {
+    return new Promise(function(resolve, reject) {
+      bcrypt.compare(password, hashed, function(error, result) {
+        if (error) {
+          reject('Passwords do not match');
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
+
+  comparePassword: function (pass, hashedPass) {
+    if (pass === hashedPass) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
