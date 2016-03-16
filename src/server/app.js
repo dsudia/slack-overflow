20c4aa66ac2cdcc4921d507d3d5f313b52a08330
David Sudia <dsudia@gmail.com>
2016-03-16 10:53:33 -0600

    bot sends correct messages

--- src/server/app.js
+++ src/server/app.js
@@ -45,11 +45,11 @@ rtm.on(RTM_EVENTS.MESSAGE, function(message) {
   var ansResponse = 'Thanks for asking! To answer a question, you use the / command /sflowa\nWhen a question gets asked, I will respond with the question ID. You can answer a question with the following format: #title #body #questionId.\nLike so: #Array.filter #When you filter an array, remember to return what\'s inside the callback function. The filter returns elements that match the true value of the callback. #189\nThen I\'ll respond with confirmation your answer has been posted.';
   var fullResponse = 'Thanks for asking! To use my service, you can use two slash commands.\n/sflowq will tell me to post a question.\n/sflowa will tell me to post an answer.\nThe format for a question is #title #body #tags,tags. An example would be #Trouble with array.filter function... #Hey all, I\'m having a lot of difficulty with higher-order functions. When I use array.filter, I\'m not gettting the return I\'m expecting. #javascript,functions,arrays\nWhen a question gets asked, I will respond with the question ID. You can answer a question with the following format: #title #body #questionId.\nLike so: #Array.filter #When you filter an array, remember to return what\'s inside the callback function. The filter returns elements that match the true value of the callback. #189\nThen I\'ll respond with confirmation your answer has been posted. Thanks for using Slack Overflow!';
   var instResponse = 'Howdy! You can ask me the following questions for more info: "How do I post a question on Slack Overflow?", "How do I post an answer on Slack Overflow?" and "How do I use Slack Overflow?"';
-  if (text.includes('how') && text.includes('use') && text.includes('overflow') && text.includes('question')) {
+  if (text.includes('how') && text.includes('post') && text.includes('overflow') && text.includes('question')) {
     rtm.sendMessage(quesResponse, channel, function messageSent() {
       console.log ('Question response sent!');
     });
-  } else if (text.includes('how') && text.includes('use') && text.includes('overflow') && text.includes('answer')) {
+  } else if (text.includes('how') && text.includes('post') && text.includes('overflow') && text.includes('answer')) {
     rtm.sendMessage(ansResponse, channel, function messageSent() {
       console.log('Answer response sent!');
     });
