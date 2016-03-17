cc16dea23ea42b7b57a1c3b884e30d8ee33eb2d7
David Schwartz <dbschwartz2@gmail.com>
2016-03-16 19:07:49 -0600

    fixed knex migration files to let forum database work

--- seeds/5_questions.js
+++ seeds/5_questions.js
@@ -11,13 +11,13 @@ exports.seed = function(knex, Promise) {
       user_id: knex('users').select('id').where('username', 'dsudia'),
       score: 1,
       flag_status: false,
-      assignment_id: knex('assignments').select('id').where('name', 'Introduction to Knex')}),
+      assignment_id: knex('assignments').select('id').where('name', 'form-based-authentication')}),
     knex('questions').insert({title: 'Help! I can\'t figure out CSS positioning!',
       body: 'Relative and absolute and fixed, oh my! Can anyone give me a good explanation of the differences?',
-      group_id: knex('groups').select('id').where('name', 'G18'),
+      group_id: knex('groups').select('id').where('name', 'G20'),
       user_id: knex('users').select('id').where('username', 'mhea0'),
       score: 5,
       flag_status: false,
-      assignment_id: knex('assignments').select('id').where('name', 'CSS Positioning')})
+      assignment_id: knex('assignments').select('id').where('name', 'form-based-authentication')})
   );
 };
