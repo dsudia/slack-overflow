0a31504371a01231b4fffcd9ca7b263c96656d52
David Sudia <dsudia@gmail.com>
2016-03-18 11:58:36 -0600

    queries broken out for add Question

--- src/server/routes/questionRoutes/addQuestion.js
+++ src/server/routes/questionRoutes/addQuestion.js
@@ -1,5 +1,7 @@
 var knex = require('../../../../db/knex');
 var Promise = require('bluebird');
+var quesQueries = require('../../../queries/questions');
+var tagQueries = require('../../../queries/tags');
 
 module.exports = function(req, res, next) {
   // store form info in a variable
@@ -14,15 +16,15 @@ module.exports = function(req, res, next) {
   var body = qData.body;
 
   // insert question data into questions table, get question's ID back
-  return knex('questions').insert({
-    title: qData.title,
-    body: body,
-    group_id: qData.group_id,
-    user_id: req.user.id,
-    score: 0,
-    flag_status: false,
-    assignment_id: qData.assignment_id
-  }, 'id').then(function(id) {
+  quesQueries.addQuestion(
+      qData.title,
+      body,
+      qData.group_id,
+      req.user.id,
+      0,
+      false,
+      qData.assignment_id)
+  .then(function(id) {
     // store question ID in variable for later usage
     questionId = id;
     questionId = Number(questionId);
@@ -30,13 +32,12 @@ module.exports = function(req, res, next) {
     // put tags into tags table and store ids in an array
     var promisesArray = tagArray.map(function(el, ind, arr) {
       // see if tag is already in table
-      return knex('tags').select('id').where('tag_name', el)
+      return tagQueries.searchTags(el)
       .then(function(data) {
         // if not, insert it and then put id into tagIds array
         if(data[0] === undefined) {
-          return knex('tags').insert({
-            tag_name: el
-          }, 'id').then(function(id) {
+          return tagQueries.insertTagsToTags
+          .then(function(id) {
             return tagIds.push(id);
           });
         // if so, put the id into the array.
@@ -50,10 +51,7 @@ module.exports = function(req, res, next) {
   }).then(function() {
     // insert question/tag relationships into question_tags table
     var promisesArray = tagIds.map(function(el, ind, arr) {
-      return knex('question_tags').insert({
-        question_id: questionId,
-        tag_id: el
-      });
+      return tagQueries.insertTagsToQT(questionId, el);
     });
     return Promise.all(promisesArray);
   }).then(function(data) {
