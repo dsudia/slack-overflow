dc8fcef05bab9ffa177546c2ac5f38f3b07c8edb
David Sudia <dsudia@gmail.com>
2016-03-23 11:50:13 -0600

    removed title from question table schema, changed body to text

--- migrations/20160310134650_create_questions_table.js
+++ migrations/20160310134650_create_questions_table.js
@@ -2,8 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('questions', function(table) {
     table.increments();
-    table.string('title');
-    table.string('body', 3000);
+    table.string('text');
     table.integer('group_id').references('groups.id');
     table.integer('user_id').references('users.id');
     table.integer('score');
