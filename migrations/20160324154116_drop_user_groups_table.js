6c33a80aa939b501f815902d87ba71e822b22536
David Sudia <dsudia@gmail.com>
2016-03-24 16:48:20 -0600

    fixed seed file so rollbacks work

--- migrations/20160324154116_drop_user_groups_table.js
+++ migrations/20160324154116_drop_user_groups_table.js
@@ -4,5 +4,9 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.createTable('user_groups');
+  return knex.schema.createTable('user_groups', function(table) {
+    table.increments();
+    table.integer('user_id').references('users.id');
+    table.integer('group_id').references('groups.id');
+  });
 };
