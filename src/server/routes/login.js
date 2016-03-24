e8f453cbb47cc05e9c9a6b9590a92982fc746701
David Sudia <dsudia@gmail.com>
2016-03-24 17:14:28 -0600

    same as previous commit, only for real

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var helpers = require('../lib/helpers');
 
-router.get('/', function(req, res, next) {
+router.get('/', helpers.loginRedirect, function(req, res, next) {
   res.render('login', {
     user: req.user,
     message: req.flash('danger')
