20fecc9b381322e9f7576178366ef9490c3e5c35
David Sudia <dsudia@gmail.com>
2016-03-24 17:32:46 -0600

    update question seed file to accurately work with seeded user

--- seeds/5_questions.js
+++ seeds/5_questions.js
@@ -7,13 +7,13 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('questions').insert({body: 'Hey class. I\'m trying to select all the entries from a table using a where statement, and I can\'t figure out how to do it. I\'ve tried knex.select().where(\'name\', Dave). It\'s not working. Help!',
       group_id: knex('groups').select('id').where('name', 'G19'),
-      user_id: knex('users').select('id').where('github_login', 'dsudia'),
+      user_id: knex('users').select('id').where('email', 'dsudia@gmail.com'),
       score: 1,
       flag_status: false,
       assignment_id: knex('assignments').select('id').where('name', 'form-based-authentication')}),
     knex('questions').insert({body: 'Relative and absolute and fixed, oh my! Can anyone give me a good explanation of the differences?',
       group_id: knex('groups').select('id').where('name', 'G20'),
-      user_id: knex('users').select('id').where('github_login', 'dsudia'),
+      user_id: knex('users').select('id').where('email', 'dsudia@gmail.com'),
       score: 5,
       flag_status: false,
       assignment_id: knex('assignments').select('id').where('name', 'form-based-authentication')})
